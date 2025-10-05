62060d272b1c80dca097268acb6cab3d02129a80
Avik10 <bnk.avik@gmail.com>
2025-10-05 23:35:38 +0530

    cors fix

--- Backend/socket.js
+++ Backend/socket.js
@@ -8,7 +8,8 @@ const initializeSocket = (server) => {
     io = new Server(server, {
         cors: {
             origin: ["http://localhost:8000", "http://localhost:5173", "https://yatri-sathi.netlify.app"],
-            methods: ["GET", "POST"],
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            credentials: true
         }
     });
 
